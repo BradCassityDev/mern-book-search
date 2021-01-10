3d50a3f83b88c65503342da60ea8bb1d574da08a
Brad Cassity <bradley.cassity@gmail.com>
2021-01-10 13:47:08 -0700

    Added typeDefs and resolvers on the server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,31 +1,30 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
     type User {
         _id: ID
         username: String
         email: String
-        friendCount: Int
-        thoughts: [Thought]
-        friends: [User]
+        bookCount: Int
+        savedBooks: [Book]
     }
 
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-        reactions: [Reaction]
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
     }
 
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
     }
 
     type Auth {
@@ -35,20 +34,14 @@ const typeDefs = gql`
 
     type Query {
         me: User
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
     }
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        addThought(thoughtText: String!): Thought
-        addReaction(thoughtId: ID!, reactionBody: String!): Thought
-        addFriend(friendId: ID!): User
+        createUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput!): User
+        removeBook(bookId: String!): User
     }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
